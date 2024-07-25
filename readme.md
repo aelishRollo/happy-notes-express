dce21653b610104467893e84f0f9fa8450133b1d
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-07-25 13:29:00 -0400

    Add basic root route reply

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ What I've done:
 
 1. In the terminal: ` npm init`
 2. In the terminal: ` npm install express --save`
+
+3. Add `node_modules` to .gitignore
--- server.js
+++ server.js
@@ -6,3 +6,9 @@ const PORT = 3000
 app.listen(3000, () => {
     console.log(`Hello Wizard, we listening on ${PORT}`)
 })
+
+
+app.get('/',(req,res) => {
+    console.log('ROOT ROUTE RAIDED, RUH-ROH-RAGGY')
+    res.send('Hello, Universe')
+})
